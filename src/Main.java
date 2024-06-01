b36dc0802456932a2d18e976a7be0bfe5f70e6b0
Tamyris Torres <tamyres_wedella@hotmail.com>
2024-06-01 20:48:30 -0300

    Finish project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -30,5 +32,33 @@ public class Main {
         mentoria.setData(LocalDate.from(LocalDate.now()));
 
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Santander");
+        bootcamp.setDescricao("Estando Java com Santander e a Dio");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        System.out.println("------------");
+        Dev dev = new Dev();
+        dev.setNome("Tamyris");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + dev.getConteudoInscritos());
+        dev.progredir();
+        dev.progredir();
+        System.out.println("Conteudos concluidos" + dev.getConteudoConcluidos());
+        System.out.println("Total XP " +  dev.calcularTotalXp());
+
+        System.out.println("------------");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("João");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + dev2.getConteudoInscritos());
+        dev2.progredir();
+        System.out.println("Conteudos Concluidos" + dev2.getConteudoConcluidos());
+        System.out.println("Total XP " +  dev2.calcularTotalXp());
     }
 }
